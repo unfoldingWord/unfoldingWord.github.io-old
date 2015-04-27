c4053412cb8f85f4ed351a2488b3f159433297df
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2015-04-27 16:56:57 -0400

    OBS support

--- getLatestOBS.py
+++ getLatestOBS.py
@@ -39,36 +39,58 @@ def get_language_list():
             print 'Error ' + str(response.status) + ': ' + response.reason
             return None
 
-        resptext = response.read()
+        langlist = json.loads(response.read())
 
-    with open('_data/obs-catalog.json', 'w') as out:
-        out.write(resptext)
+    # sort the list of languages by language code
+    langlist.sort(key=lambda x: x['language'])
 
-    return json.loads(resptext)
+    with open('_data/obs-catalog.json', 'w') as catalogout:
+        catalogout.write(json.dumps(langlist))
+
+    return langlist
 
 
-def get_language_file(langcode):
+def get_language_file(langcodestr):
 
     with SelfClosingConnection('api.unfoldingword.org') as connection:
 
-        connection.request('GET', '/obs/txt/1/' + langcode + '/obs-' + langcode + '.json')
+        connection.request('GET', '/obs/txt/1/' + langcodestr + '/obs-' + langcodestr + '.json')
         response = connection.getresponse()
 
         # check the status
         if response.status != 200:
-            print 'Error retrieving ' + langcode + ': ' + str(response.status) + ': ' + response.reason
+            print 'Error retrieving ' + langcodestr + ': ' + str(response.status) + ': ' + response.reason
             return
 
         resptext = response.read()
 
-        with open('_data/obs-' + langcode + '.json', 'w') as out:
-            out.write(resptext)
+        with open('_data/obs-' + langcodestr + '.json', 'w') as langfileout:
+            langfileout.write(resptext)
 
         return json.loads(resptext)
 
 
-def list_to_jekyll_string(value):
-    return str(value).replace("', u'", "', '").replace("[u'", "['")
+def get_front_matter(code, check, chapnum, chapname, chaptitle, chaps, frames, words, imgsize, nextone):
+
+    returnval = '---\n'
+    returnval += 'layout: obs_reveal\n'
+    returnval += 'title: unfoldingWord Open Bible Stories\n'
+    returnval += 'languageCode: ' + code + '\n'
+    returnval += 'checkingLevel: ' + check + '\n'
+    returnval += 'chapterNumber: ' + chapnum + '\n'
+    returnval += 'chapterName: ' + chapname + '\n'
+    returnval += 'chapterTitle: ' + chaptitle + '\n'
+    returnval += 'chapters: ' + chaps + '\n'
+    returnval += 'frames: ' + frames + '\n'
+    returnval += 'appWords: ' + words + '\n'
+    returnval += 'imageSize: ' + imgsize + '\n'
+
+    if nextone is not None:
+        returnval += 'nextChapter: ' + nextone + '\n'
+
+    returnval += '---\n'
+
+    return returnval
 
 
 if __name__ == '__main__':
@@ -80,6 +102,8 @@ if __name__ == '__main__':
     for language in languages:
 
         langcode = language['language']
+        checklevel = language['status']['checking_level']
+
         print 'Retrieving ' + langcode
 
         # retrieve from api.unfoldingword.org
@@ -89,26 +113,48 @@ if __name__ == '__main__':
         if os.path.isdir(langcode):
             shutil.rmtree(langcode)
 
-        os.makedirs(langcode)
+        path360 = os.path.join(langcode, '360px')
+        path2160 = os.path.join(langcode, '2160px')
+
+        os.makedirs(path360)
+        os.makedirs(path2160)
 
         # get chapter names
         chapters = []
-        chapterTitles = []
+        counter = 1
         for chapter in obs['chapters']:
-            chapters.append(chapter['title'])
-            chapterTitles.append(chapter['ref'])
+            chapters.append([format(counter, '02d'), chapter['title']])
+            counter += 1
+        chapterstr = json.dumps(chapters)
 
-        chapterstr = list_to_jekyll_string(chapters)
-        titlestr = list_to_jekyll_string(chapterTitles)
+        # get i18n
+        appwords = json.dumps(obs['app_words'])
 
-        # create the markdown file
-        with open(langcode + '/index.html', 'w') as out:
-            out.write('---\n')
-            out.write('layout: obs_reveal\n')
-            out.write('title: unfoldingWord Open Bible Stories\n')
-            out.write('chapters: ' + chapterstr + '\n')
-            out.write('chapterTitles: ' + titlestr + '\n')
-            out.write('---\n')
+        # get chapter data
+        nextchap = None
+        for chapter in reversed(obs['chapters']):
+
+            # get the chapter number
+            chapterNum = chapter['frames'][0]['id'].split('-')[0]
+            chapterDir360 = os.path.join(path360, chapterNum)
+            chapterDir2160 = os.path.join(path2160, chapterNum)
+            os.makedirs(chapterDir360)
+            os.makedirs(chapterDir2160)
+
+            # create the 360px markdown file
+            with open(os.path.join(chapterDir360, 'index.html'), 'w') as out:
+                out.write(get_front_matter(langcode, checklevel, chapterNum,
+                                           json.dumps(chapter['title']), json.dumps(chapter['ref']),
+                                           chapterstr, json.dumps(chapter['frames']), appwords, '360', nextchap))
+
+            # create the 2160px markdown file
+            with open(os.path.join(chapterDir2160, 'index.html'), 'w') as out:
+                out.write(get_front_matter(langcode, checklevel, chapterNum,
+                                           json.dumps(chapter['title']), json.dumps(chapter['ref']),
+                                           chapterstr, json.dumps(chapter['frames']).replace("360px", "2160px"),
+                                           appwords, '2160', nextchap))
+
+            nextchap = chapterNum
 
     print ''
     print 'Finished'
