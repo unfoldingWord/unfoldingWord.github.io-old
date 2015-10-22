eec5f278b50ece7f577e9386913062812406c4ca
Codemis <johnathan@jpulos.com>
2015-10-22 16:00:15 -0700

    Completed the OBS page.

--- js/application.js
+++ js/application.js
@@ -1,10 +1,4 @@
 /**
- * Store sitewide Javascript in this file
- * 
- * @type {String}
- */
-var langProgressUrl = 'https://door43.org/{0}/obs';
-/**
  * The path for the checking image
  *
  * @type {String}
@@ -52,6 +46,38 @@ function setBaseUrl(baseUrl) {
   siteBaseUrl = baseUrl;
 };
 /**
+ * Remove any data that is not a language (The feed for in progress translations sends a date_modified in the same array)
+ *
+ * @param  {Array} languages The languages array to clean
+ *
+ * @return {Array}           A cleaned array
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ */
+function cleanLanguages(languages) {
+  var cleaned = [];
+
+  for (var i = 0; i < languages.length; i++) {
+    if ('lc' in languages[i]) {
+      cleaned.push(languages[i]);
+    }
+  }
+  return cleaned;
+};
+/**
+ * Array.sort function for sorting by the given language code
+ *
+ * @param  {Object} a The first JSON object to compare
+ * @param  {Object} b The second object to 
+ *
+ * @return {Integer}  The order to sort in
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ */
+function sortByLanguageCode(a, b) {
+  return a.language_code === b.language_code ? 0 : +(a.language_code > b.language_code) || -1;
+};
+/**
  * Setup the search for translations
  *
  * @param  {Array} fallbackData An array of JSON Objects of translated data to be used if we cannot access the API data
@@ -89,7 +115,8 @@ function setupSearchTranslations(fallbackData) {
       });
       $.getJSON('https://api.unfoldingword.org/obs/txt/1/obs-in-progress.json')
         .done(function(translations) {
-          $.each(translations, function(index, translation) {
+          cleanedTranslations = cleanLanguages(translations);
+          $.each(cleanedTranslations, function(index, translation) {
             var data = {
               'language_code':      translation.lc,
               'language_direction': '',
@@ -100,9 +127,7 @@ function setupSearchTranslations(fallbackData) {
             retrievedTranslations.push(data);
           });
           // sort by language code
-          retrievedTranslations.sort(function(a, b) {
-            return a.language_code === b.language_code ? 0 : +(a.language_code > b.language_code) || -1;
-          });
+          retrievedTranslations.sort(sortByLanguageCode);
           currentTranslations = retrievedTranslations;
           displayTranslations(currentTranslations);
         })
@@ -196,157 +221,5 @@ function displayTranslations(translations) {
     }
     resultsElement.append(translationParentElement);
     translationElement = null;
-  }
-};
-
-
-
-
-
-
-
-
-
-
-
-/**
- * Setup the Available translations
- *
- * @param  {Array} stories An array of available translations JSON objects (This is a backup if we cannot get the API)
- * ex. {"language":"am","string":"አማርኛ","status": {"version":"3.3.1","checking_level":"3"}}
- *
- * @return {null}
- *
- * @author Johnathan Pulos <johnathan@missionaldigerati.org>
- */
-function setupAvailableTranslations(stories) {
-  /**
-   * Try to get the list of story languages from api.unfoldingword.org. If not successful, use the saved list.
-   */
-  $.getJSON('https://api.unfoldingword.org/obs/txt/1/obs-catalog.json')
-    .done(function(data) {
-      setStoryList(data);
-    })
-    .fail(function() {
-      setStoryList(stories);
-    });
-}
-/**
- * Setup the in progress OBS stories
- *
- * @param  {Array} languages An array of languages that are in progress JSON Object (This is a backup if we cannot get the API)
- * ex. {"lc": "aaa", "ln": "Ghotuo"}
- *
- * @return {null}
- *
- * @author Johnathan Pulos <johnathan@missionaldigerati.org>
- */
-function setupInProgressTranslations(languages) {
-  /**
-   * Try to get the list of story languages from api.unfoldingword.org. If not successful, use the saved list.
-   */
-  $.getJSON('https://api.unfoldingword.org/obs/txt/1/obs-in-progress.json')
-    .done(function(data) {
-      setInProgressList(data);
-    })
-    .fail(function() {
-      setInProgressList(languages);
-    });
-}
-/**
- * Setup the in progress list for OBS stories in progress
- *
- * @param  {Array} languages An array of JSON objects representing each language in progress
- * ex. {"lc": "aaa", "ln": "Ghotuo"}
- *
- * @author Johnathan Pulos <johnathan@missionaldigerati.org>
- */
-function setInProgressList(languages) {
-  var cleanedLanguages = cleanLanguages(languages);
-  // sort by language code
-  cleanedLanguages.sort(function(a, b) {
-    return a.lc === b.lc ? 0 : +(a.lc > b.lc) || -1;
-  });
-
-  var chunkedLanguages = arrayToChuncks(cleanedLanguages, 3);
-  var template = $('#obs-in-progress-template');
-
-  // Issue #100, 13 OCT 2015, Phil Hopper: check for valid data
-  var reCheckLanguageCode = /^[a-z]{2}/;  // all language codes start with at least 2 lower-case characters
-
-  for (var i = 0; i < chunkedLanguages.length; i++) {
-    var langs = chunkedLanguages[i];
-    var elementNumber = i+1;
-    var ul = $('ul#obs-in-progress-list-' + elementNumber);
-    for (var l = 0; l < langs.length; l++) {
-      var langCode = langs[l].lc;
-
-      // Issue #100, 13 OCT 2015, Phil Hopper: check for valid data
-      if (!reCheckLanguageCode.test(langCode)) {
-        continue;
-      }
-
-      var li = $(template.html());
-
-      var span = li.find('span[data-span-type="langString"]');
-      span.attr('lang', langCode).html(langs[l].ln);
-
-      var link = li.find('a[data-link-type="langLink"]');
-      link.attr('href', langProgressUrl.format(langCode));
-
-      // language code
-      li.find('span[data-span-type="langLanguage"]').html(langCode);
-
-      ul.append(li);
-    }
-  }
-}
-/**
- * Setup the story list for OBS translations
- *
- * @param  {Array} stories An array of story objects
- * ex. {"language":"am","string":"አማርኛ","status": {"version":"3.3.1","checking_level":"3"}}
- *
- * @author Johnathan Pulos <johnathan@missionaldigerati.org>
- */
-function setStoryList(stories) {
-
-  // sort by language code
-  stories.sort(function(a, b) {
-    return a.language === b.language ? 0 : +(a.language > b.language) || -1;
-  });
-
-  // get the template for story items
-  var body = $('body');
-  var ul = body.find('#story-languages');
-  var template = body.find('#obs-language-template');
-
-  for (var i=0; i<stories.length; i++) {
-
-    var langCode = stories[i].language;
-
-    // create a new li
-    var li = $(template.html());
-
-    // language name
-    var span = li.find('span[data-span-type="langString"]');
-    span.attr('lang', langCode);
-    span.html(stories[i].string);
-
-    // language code
-    li.find('span[data-span-type="langLanguage"]').html(langCode);
-
-    // link URLs
-    var links = li.find('a.img_swap');
-    links[0].href = 'https://api.unfoldingword.org/obs/txt/1/' + langCode + '/slides/360px/01/';
-    links[1].href = 'https://api.unfoldingword.org/obs/txt/1/' + langCode + '/slides/2160px/01/';
-    links[2].href = 'https://api.unfoldingword.org/obs/txt/1/' + langCode + '/obs-' + langCode + '-v' + stories[i].status.version.replace(/\./g, '_') + '.pdf';
-    // links[3].href = 'https://api.unfoldingword.org/obs/txt/1/' + langCode + '/obs-' + langCode + '-' + stories[i].status.publish_date + '.docx';
-    // links[4].href = 'https://api.unfoldingword.org/obs/txt/1/' + langCode + '/obs-' + langCode + '-' + stories[i].status.publish_date + '.odt';
-
-    // checking level
-    li.addClass('checking-level-' + stories[i].status.checking_level);
-
-    ul.append(li);
   }
 };
