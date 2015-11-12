7134ba82589efa7184e18e307c6123bb6c92659b
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2015-11-12 10:54:03 -0500

    Rollback changes to tA

--- js/application.js
+++ js/application.js
@@ -27,7 +27,7 @@ String.prototype.format = function() {
  * @param  {Array} stories An array of available translations JSON objects (This is a backup if we cannot get the API)
  * ex. {"language":"am","string":"አማርኛ","status": {"version":"3.3.1","checking_level":"3"}}
  *
- * @return {null}
+ * @return {Void}
  *
  * @author Johnathan Pulos <johnathan@missionaldigerati.org>
  */
@@ -42,14 +42,14 @@ function setupAvailableTranslations(stories) {
     .fail(function() {
       setStoryList(stories);
     });
-}
+};
 /**
  * Setup the in progress OBS stories
  *
  * @param  {Array} languages An array of languages that are in progress JSON Object (This is a backup if we cannot get the API)
  * ex. {"lc": "aaa", "ln": "Ghotuo"}
  *
- * @return {null}
+ * @return {Void}
  *
  * @author Johnathan Pulos <johnathan@missionaldigerati.org>
  */
@@ -64,7 +64,7 @@ function setupInProgressTranslations(languages) {
     .fail(function() {
       setInProgressList(languages);
     });
-}
+};
 /**
  * Setup the in progress list for OBS stories in progress
  *
@@ -107,14 +107,14 @@ function setInProgressList(languages) {
       link.attr('href', langProgressUrl.format(langCode));
 
       // language code
-      li.find('span[data-span-type="langLanguage"]').html(langCode);
+      li.find('span[data-span-type="langLanguage"]').html(langCode)
 
       ul.append(li);
     }
   }
-}
+};
 /**
- * Setup the story list for OBS translations
+ * Setup the story ist for OBS translations
  *
  * @param  {Array} stories An array of story objects
  * ex. {"language":"am","string":"አማርኛ","status": {"version":"3.3.1","checking_level":"3"}}
@@ -161,7 +161,7 @@ function setStoryList(stories) {
 
     ul.append(li);
   }
-}
+};
 /**
  * Remove any data that is not a language (The feed sends a date_modified in the same array)
  *
@@ -180,12 +180,12 @@ function cleanLanguages(languages) {
     }
   }
   return cleaned;
-}
+};
 /**
  * Break an array (data) into n (total) number of arrays
  *
  * @param  {Array}    data  The array to break up
- * @param  {int}  total How many arrays you want back
+ * @param  {Integer}  total How many arrays you want back
  *
  * @return {Array}          An array of all the seperate arrays
  *
@@ -198,129 +198,4 @@ function arrayToChuncks(data, total) {
     final.push(data.splice(0, size));
   }
   return final;
-}
-
-/**
- * Gets the translation academy json from the api endpoint
- */
-function getTranslationAcademy() {
-
-  // get the tA text from the tD endpoint
-  $.getJSON('http://td-demo.unfoldingword.org/publishing/ta-en.json')
-      .done(function(data) {
-        buildTranslationAcademy(data);
-      })
-      .fail(function(jqXHR) {
-
-        // remove the Loading message
-        $('#loading-h3').text('');
-
-        // let the user know there was an error
-        var msg = 'Not able to load translationAcademy';
-        var code = jqXHR['status'];
-
-        // a CORS error will not return a status, and the statusText will just be 'error'
-        if (code) {
-          msg += ': ' + code + ', ' + jqXHR['statusText'];
-        }
-        alert(msg);
-
-        // log the response for debugging
-        console.log(jqXHR);
-      });
-}
-
-/**
- * Uses the translation academy data to build the tA page
- *
- * @param {object} data
- */
-function buildTranslationAcademy(data) {
-
-  // remove the Loading message
-  $('#loading-h3').text('');
-
-  var pagesDiv = $('#ta-pages-div');
-  var chapters = data['chapters'];
-  var sections = {};
-
-  // first section is the TOC, which we don't need, so start at index 1
-  for (var i = 1; i < chapters.length; i++) {
-    for (var j = 0; j < chapters[i]['frames'].length; j++) {
-
-      // add the text to the page
-      var currentPage = $('<div class="bs-docs-section"></div>');
-      currentPage.html(chapters[i]['frames'][j]['text']);
-      pagesDiv.append(currentPage);
-
-      // get the section info for the TOC
-      var ref = chapters[i]['ref'].split('/');
-      var section_name = ref[ref.length - 2];
-      if (!sections[section_name]) {
-        sections[section_name] = [];
-      }
-
-      // add the page id to the list for the TOC
-      sections[section_name].push([chapters[i]['frames'][j]['id'], chapters[i]['title']]);
-    }
-  }
-
-  // now build the TOC
-  var tocUl = $('#ta-toc-ul');
-
-  // remember any existing items
-  var existing = tocUl.html();
-  tocUl.html('');
-
-  for (var key in sections) {
-
-    if (sections.hasOwnProperty(key)) {
-
-      // build the section heading
-      var currentSection = $('<li><a href="#' + sections[key][0][0] + '">' + getSectionTitle(key) + '</a></li>');
-      var subItems = $('<ul class="nav"><ul></ul>');
-
-      // add the sub-items
-      for (var k = 0; k < sections[key].length; k++) {
-        console.log(sections[key][k]);
-        subItems.append($('<li><a href="#' + sections[key][k][0] + '">' + sections[key][k][1] + '</a></li>'));
-      }
-
-      currentSection.append(subItems);
-      tocUl.append(currentSection);
-    }
-  }
-
-  // now add existing items back in
-  if (existing) {
-    tocUl.append($(existing));
-  }
-
-}
-
-/**
- * Get the title to display for th given key
- * @param {string} key
- */
-function getSectionTitle(key) {
-
-  switch (key.substr(0, 4)) {
-    case 'intr':
-      return 'Introduction';
-
-    case 'tran':
-      return 'Translation';
-
-    case 'chec':
-      return 'Checking';
-
-    case 'tech':
-      return 'Technology';
-
-    case 'proc':
-      return 'Process';
-
-    default:
-      return 'None';
-  }
-}
+};
