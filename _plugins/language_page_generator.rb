52b14ae7a0c2404d0cc828f97d7ccae9231f8424
Codemis <johnathan@jpulos.com>
2015-12-03 15:30:59 -0800

    Changed the layout of the languages page to match the new design.

--- _plugins/language_page_generator.rb
+++ _plugins/language_page_generator.rb
@@ -57,10 +57,15 @@ module Jekyll
     # 
     def initialize
       @languages = []
-      @low_res_video_url  = 'https://api.unfoldingword.org/obs/txt/1/%s/slides/360px/01/'
-      @high_res_video_url = 'https://api.unfoldingword.org/obs/txt/1/%s/slides/2160px/01/'
-      @pdf_url            = 'https://api.unfoldingword.org/obs/txt/1/%s/obs-%s-v%s.pdf'
-      @checking_image_url = '/assets/img/uW-Level%s-64px.png'
+      @low_res_slideshow_url  = 'https://api.unfoldingword.org/obs/txt/1/%s/slides/360px/01/'
+      @high_res_slideshow_url = 'https://api.unfoldingword.org/obs/txt/1/%s/slides/2160px/01/'
+      @pdf_url                = 'https://api.unfoldingword.org/obs/txt/1/%s/obs-%s-v%s.pdf'
+      @low_res_audio_url      = 'https://api.unfoldingword.org/obs/mp3/1/%s/%s_obs_v%s_mp3_32kbps.zip'
+      @med_res_audio_url      = 'https://api.unfoldingword.org/obs/mp3/1/%s/%s_obs_v%s_mp3_64kbps.zip'
+      @high_res_audio_url     = 'https://api.unfoldingword.org/obs/mp3/1/%s/%s_obs_v%s_mp3_128kbps.zip'
+      @low_res_video_url      = 'https://api.unfoldingword.org/obs/mp4/1/%s/%s_obs_v%s_mp4_360p.zip'
+      @high_res_video_url     = 'https://api.unfoldingword.org/obs/mp4/1/%s/%s_obs_v%s_mp4_720p.zip'
+      @checking_image_url     = '/assets/img/uW-Level%s-64px.png'
       set_language_data()
       set_languages()
     end
@@ -127,9 +132,20 @@ module Jekyll
         # ASSUMPTION: There is only 1 version in the vers array?
         # 
         if slug == 'obs'
+          # Hack until they add these resources to the feed
+          # 
           data = {
-            'low_res_video_url'       =>  @low_res_video_url % [code],
-            'high_res_video_url'      =>  @high_res_video_url % [code],
+            'audio_urls'              =>  {
+              'low'   =>  (code == 'en') ? @low_res_audio_url % [code, code, version] : '',
+              'med'   =>  (code == 'en') ? @med_res_audio_url % [code, code, version] : '',
+              'high'  =>  (code == 'en') ? @high_res_audio_url % [code, code, version] : ''
+            },
+            'video_urls'              => {
+              'low'   =>  (code == 'en') ? @low_res_video_url % [code, code, version] : '',
+              'high'  =>  (code == 'en') ? @high_res_video_url % [code, code, version] : ''
+            },
+            'low_res_slideshow_url'   =>  @low_res_slideshow_url % [code],
+            'high_res_slideshow_url'  =>  @high_res_slideshow_url % [code],
             'pdf_url'                 =>  @pdf_url % [code, code, version],
             'checking_level'          =>  status['checking_level'],
             'checking_level_image'    =>  @checking_image_url % [status['checking_level']]
