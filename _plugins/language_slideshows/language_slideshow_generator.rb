26b9afc0f2781ee2e9997c24ff284b9386a4bd73
Codemis <johnathan@jpulos.com>
2016-02-03 14:01:26 -0800

    Added a python command configuration for easier setting.

--- _plugins/language_slideshows/language_slideshow_generator.rb
+++ _plugins/language_slideshows/language_slideshow_generator.rb
@@ -8,9 +8,13 @@ module Jekyll
       puts ''
       puts 'Running the Slideshow Generator (Requires Python 2)'
       puts '---------------------------------------------------'
-      output = `python2 #{generate_script}`
+      python_cmd =  Jekyll.configuration({})['python_command']
+      if python_cmd.nil? || python_cmd.empty?
+        python_cmd = 'python2'
+      end
+      output = `#{python_cmd} #{generate_script}`
       puts output
-      puts ''
+      puts 'Finished!'
     end
 
   end
