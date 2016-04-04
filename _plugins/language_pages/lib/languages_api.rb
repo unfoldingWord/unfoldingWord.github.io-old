1d59c736dece26c539f029737c60be0109530973
Jesse Griffin <jesse@unfoldingword.org>
2016-04-04 16:32:55 +0700

    Fixes #128, switches to cdn for media zip downloads

--- _plugins/language_pages/lib/languages_api.rb
+++ _plugins/language_pages/lib/languages_api.rb
@@ -11,11 +11,11 @@ class LanguagesAPI
     @low_res_slideshow_url  = '/%s/slides/360px/01/'
     @high_res_slideshow_url = '/%s/slides/2160px/01/'
     @pdf_url                = 'https://api.unfoldingword.org/obs/txt/1/%s/obs-%s-v%s.pdf'
-    @low_res_audio_url      = 'https://api.unfoldingword.org/obs/mp3/1/%s/%s_obs_v%s_mp3_32kbps.zip'
-    @med_res_audio_url      = 'https://api.unfoldingword.org/obs/mp3/1/%s/%s_obs_v%s_mp3_64kbps.zip'
-    @high_res_audio_url     = 'https://api.unfoldingword.org/obs/mp3/1/%s/%s_obs_v%s_mp3_128kbps.zip'
-    @low_res_video_url      = 'https://api.unfoldingword.org/obs/mp4/1/%s/%s_obs_v%s_mp4_360p.zip'
-    @high_res_video_url     = 'https://api.unfoldingword.org/obs/mp4/1/%s/%s_obs_v%s_mp4_720p.zip'
+    @low_res_audio_url      = 'https://cdn.unfoldingword.org/%s/obs/v4/32kbps/%s_obs_32kbps.zip'
+    @med_res_audio_url      = 'https://cdn.unfoldingword.org/%s/obs/v4/64kbps/%s_obs_64kbps.zip'
+    @high_res_audio_url     = 'https://cdn.unfoldingword.org/%s/obs/v4/128kbps/%s_obs_128kbps.zip'
+    @low_res_video_url      = 'https://cdn.unfoldingword.org/%s/obs/v4/360p/%s_obs_360p.zip'
+    @high_res_video_url     = 'https://cdn.unfoldingword.org/%s/obs/v4/720p/%s_obs_720p.zip'
     @checking_image_url     = '/assets/img/uW-Level%s-64px.png'
     set_language_data()
     set_languages()
