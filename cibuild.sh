61ede0087ce3113fca355d683dfbd797a23a6f53
Jesse Griffin <jag3773@gmail.com>
2017-02-07 14:09:55 -0700

    set htmlproofer to look for remote images if assets dir is missing

--- cibuild.sh
+++ cibuild.sh
@@ -8,7 +8,13 @@ bundle exec mdl ./pages -r MD001,MD003,MD004,MD005,MD006,MD007,MD009,MD010,MD011
 bundle exec jekyll build
 
 # Run HTML Proofer against built site
-bundle exec htmlproofer ./_site --disable-external --assume-extension --check-html --allow-hash-href --file-ignore ./_site/academy/ta-intro.html,./_site/academy/ta-process.html,./_site/academy/ta-translation-2.html,./_site/gaj-x-ymnk/slides/360px/37/index.html,./_site/gaj-x-ymnk/slides/360px/18/index.html,./_site/gaj-x-ymnk/slides/2160px/37/index.html,./_site/gaj-x-ymnk/slides/2160px/18/index.html --alt-ignore /.*\/ta/jpg\/.*/,
+# Prefers to check against a local assets folder if available,
+# otherwise it will check against the test S3 bucket.
+if [ -d ./_site/assets ]; then
+  bundle exec htmlproofer ./_site --disable-external --assume-extension --check-html --allow-hash-href --file-ignore ./_site/academy/ta-intro.html,./_site/academy/ta-process.html,./_site/academy/ta-translation-2.html,./_site/gaj-x-ymnk/slides/360px/37/index.html,./_site/gaj-x-ymnk/slides/360px/18/index.html,./_site/gaj-x-ymnk/slides/2160px/37/index.html,./_site/gaj-x-ymnk/slides/2160px/18/index.html --alt-ignore /.*\/ta/jpg\/.*/
+else
+  bundle exec htmlproofer ./_site --disable-external --assume-extension --check-html --allow-hash-href --file-ignore ./_site/academy/ta-intro.html,./_site/academy/ta-process.html,./_site/academy/ta-translation-2.html,./_site/gaj-x-ymnk/slides/360px/37/index.html,./_site/gaj-x-ymnk/slides/360px/18/index.html,./_site/gaj-x-ymnk/slides/2160px/37/index.html,./_site/gaj-x-ymnk/slides/2160px/18/index.html --alt-ignore /.*\/ta/jpg\/.*/ --url-swap "\/assets\/.*:http://test-unfoldingword.org.s3-website-us-west-2.amazonaws.com/assets/"
+fi
 
 # Show files in _site directory
 echo "Running ls -lh _site/en/"
