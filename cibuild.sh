d5ab8e29f686befbec1cec0f08957015e4b27bfd
Jesse Griffin <jag3773@gmail.com>
2017-02-07 13:01:54 -0700

    Enabled htmlproofer in build script

--- cibuild.sh
+++ cibuild.sh
@@ -8,7 +8,7 @@ bundle exec mdl ./pages -r MD001,MD003,MD004,MD005,MD006,MD007,MD009,MD010,MD011
 bundle exec jekyll build
 
 # Run HTML Proofer against built site
-#bundle exec htmlproofer ./_site --disable-external --assume-extension --check-html
+bundle exec htmlproofer ./_site --disable-external --assume-extension --check-html --allow-hash-href --file-ignore ./_site/academy/ta-intro.html,./_site/academy/ta-process.html,./_site/academy/ta-translation-2.html,./_site/gaj-x-ymnk/slides/360px/37/index.html,./_site/gaj-x-ymnk/slides/360px/18/index.html,./_site/gaj-x-ymnk/slides/2160px/37/index.html,./_site/gaj-x-ymnk/slides/2160px/18/index.html --alt-ignore /.*\/ta/jpg\/.*/,
 
 # Show files in _site directory
 echo "Running ls -lh _site/en/"
